7283593683b14290360088396b04028cabf549ae
hien123454 <ngochien15092000@gmail.com>
2021-10-30 22:44:23 +0700

    commit call-service

---
+++ src/main/java/com/example/user/VO/Department.java
@@ -0,0 +1,14 @@
+package com.example.user.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    private Long Id;
+    private String DepartmentName;
+    private double price;
+}
--- src/main/java/com/example/user/service/UserService.java
+++ src/main/java/com/example/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.example.user.service;
 
+import com.example.user.VO.Department;
+import com.example.user.VO.ResponseTemplateVO;
 import com.example.user.authen.UserPrincipal;
 import com.example.user.entity.User;
 import com.example.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,11 +18,11 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-
+    @Autowired
+    private RestTemplate restTemplate;
     public User createUser(User user) {
         return userRepository.saveAndFlush(user);
     }
-
 
     public UserPrincipal findByUsername(String username) {
         User user = userRepository.findByUsername(username);
@@ -43,6 +46,15 @@ public class UserService {
 
         return userPrincipal;
 
+    }
+
+    public ResponseTemplateVO getUserWithDepartment(Long userId){
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        User user = userRepository.findById(userId).get();
+        vo.setUser(user);
+        Department department =restTemplate.getForObject("http://localhost:9001/department/"+user.getDepartmentId(), Department.class);
+        vo.setDepartment(department);
+        return vo;
     }
 
 
