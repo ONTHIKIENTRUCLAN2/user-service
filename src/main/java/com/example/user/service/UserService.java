4e8dfabe84a526824d09cfe475cb0718a9a68e35
hien123454 <ngochien15092000@gmail.com>
2021-10-30 23:26:24 +0700

    commit Retry

--- src/main/java/com/example/user/service/UserService.java
+++ src/main/java/com/example/user/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.user.VO.ResponseTemplateVO;
 import com.example.user.authen.UserPrincipal;
 import com.example.user.entity.User;
 import com.example.user.repository.UserRepository;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -48,6 +49,7 @@ public class UserService {
 
     }
 
+    @Retry(name="basic")
     public ResponseTemplateVO getUserWithDepartment(Long userId){
         ResponseTemplateVO vo = new ResponseTemplateVO();
         User user = userRepository.findById(userId).get();
